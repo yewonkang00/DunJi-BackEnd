8124b6f5ad45bcdf4fed92ad0d38adebd9026ad0
jeeheaG <jeehea99@naver.com>
2022-12-17 03:57:47 +0900

    Style : First commit. Make Project

---
+++ src/test/java/com/example/dunjibackend/DunJiBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dunjibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunJiBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
