c7b6d5ba52e20624334e98aae3a522d3e7a4a0e8
turn <kksy2423@naver.com>
2022-12-17 19:15:49 +0900

    Feat: Swagger Config 추가

---
+++ src/main/java/com/dunji/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.dunji.backend.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    public static final String TITLE="Dunji-Rest API";
+    public static final String DESCRIPTION="Dunji API명세서";
+    public static final String VERSION="v0.0.1";
+
+    @Bean
+    public GroupedOpenApi test() {
+        return GroupedOpenApi.builder()
+                .group("test")
+                .pathsToMatch("/api/test/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title(TITLE)
+                        .description(DESCRIPTION)
+                        .version(VERSION));
+    }
+}
