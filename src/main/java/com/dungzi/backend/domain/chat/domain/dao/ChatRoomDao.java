05d415c18b9417c2bf7be3af49477f8f2a647dd2
rentalhub-mac78 <kksy2423@naver.com>
2023-01-13 17:36:48 +0900

    Feat: chatting entity Dao 생성

---
+++ src/main/java/com/dungzi/backend/domain/chat/domain/dao/ChatRoomDao.java
@@ -0,0 +1,16 @@
+package com.dungzi.backend.domain.chat.domain.dao;
+
+import com.dungzi.backend.domain.chat.domain.ChatRoom;
+import com.dungzi.backend.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ChatRoomDao extends JpaRepository<ChatRoom, UUID> {
+//    Optional<ChatRoom> findByChatRoomId(UUID chatRoomId);
+
+
+}
