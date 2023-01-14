ffad7c0ce32f456ec6913211a5f4a382ada9b40b
gyuturn <rbtjrrbals@gmail.com>
2023-01-14 20:01:43 +0900

    [DZ-67] Feat: 채팅방 생성 API 기능 추가

---
+++ src/main/java/com/dungzi/backend/domain/chat/domain/dto/ChatRoomResponseDto.java
@@ -0,0 +1,20 @@
+package com.dungzi.backend.domain.chat.domain.dto;
+
+import com.dungzi.backend.domain.chat.domain.ChatRoomType;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Data;
+
+public class ChatRoomResponseDto {
+    @Data
+    @Builder
+    public static class CreateChatRoom {
+        @JsonSerialize(using = ToStringSerializer.class)
+        @JsonDeserialize(using = FromStringDeserializer.class)
+        private UUID chatRoomId;
+    }
+}
