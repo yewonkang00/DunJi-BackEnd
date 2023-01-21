cac4e2f167c97c26f2a156a0b216fac7687c5b5b
gyuturn <rbtjrrbals@gmail.com>
2023-01-21 12:17:22 +0900

    [DZ-67] Test: 채팅방 찾기 로직 테스트
    1. 채팅방이 이미 존재하는경우
    2. 채팅방이 존재하지 않는경우

--- src/test/java/com/dungzi/backend/domain/chat/application/ChatRoomServiceTest.java
+++ src/test/java/com/dungzi/backend/domain/chat/application/ChatRoomServiceTest.java
@@ -2,6 +2,7 @@ package com.dungzi.backend.domain.chat.application;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
@@ -11,9 +12,12 @@ import com.dungzi.backend.domain.chat.dao.ChatRoomDao;
 import com.dungzi.backend.domain.chat.dao.UserChatRoomDao;
 import com.dungzi.backend.domain.chat.domain.ChatRoom;
 import com.dungzi.backend.domain.chat.domain.ChatRoomType;
+import com.dungzi.backend.domain.chat.domain.UserChatRoom;
 import com.dungzi.backend.domain.user.application.AuthService;
 import com.dungzi.backend.domain.user.dao.UserDao;
 import com.dungzi.backend.domain.user.domain.User;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,7 +43,7 @@ public class ChatRoomServiceTest {
     @Test
     @DisplayName("채팅방 생성 로직 테스트")
     public void testCreateChatRoom() {
-        // Arrange
+        // given
         String opponentNickName = "opponent";
         User ownUser = new User();
         User opponentUser = new User();
@@ -49,13 +53,58 @@ public class ChatRoomServiceTest {
         when(userDao.findByNickname(opponentNickName)).thenReturn(Optional.of(opponentUser));
         when(chatRoomDao.save(any(ChatRoom.class))).thenReturn(expectedChatRoom);
 
-        // Act
+        // when
         ChatRoom actualChatRoom = chatRoomService.createChatRoom(opponentNickName);
 
-        // Assert
+        // then
         assertEquals(expectedChatRoom, actualChatRoom);
-        verify(userChatRoomDao).save(argThat(ownUserChatRoom -> ownUserChatRoom.getChatRoom().equals(expectedChatRoom) && ownUserChatRoom.getUser().equals(ownUser) && ownUserChatRoom.getChatRoomType().equals(
-                ChatRoomType.SEEK)));
+        verify(userChatRoomDao).save(argThat(ownUserChatRoom -> ownUserChatRoom.getChatRoom().equals(expectedChatRoom) && ownUserChatRoom.getUser().equals(ownUser) && ownUserChatRoom.getChatRoomType().equals(ChatRoomType.SEEK)));
         verify(userChatRoomDao).save(argThat(opponentUserChatRoom -> opponentUserChatRoom.getChatRoom().equals(expectedChatRoom) && opponentUserChatRoom.getUser().equals(opponentUser) && opponentUserChatRoom.getChatRoomType().equals(ChatRoomType.GIVEN_OUT)));
+    }
+
+    @Test
+    @DisplayName("채팅방 존재 여부 테스트- 채팅방 존재하는 case")
+    public void findAlreadyExistRoom() {
+        // given
+        String opponentNickName = "opponent";
+        User ownUser = new User();
+        User opponentUser = new User();
+        ChatRoom expectedChatRoom = new ChatRoom();
+        when(authService.getUserFromSecurity()).thenReturn(ownUser);
+        when(userDao.findById(ownUser.getUserId())).thenReturn(Optional.of(ownUser));
+        when(userDao.findByNickname(opponentNickName)).thenReturn(Optional.of(opponentUser));
+
+        List<UserChatRoom> ownUserChatRooms = Arrays.asList(
+                UserChatRoom.builder().user(ownUser).chatRoom(expectedChatRoom).chatRoomType(ChatRoomType.SEEK).build());
+        List<UserChatRoom> opponentUserChatRooms = Arrays.asList(
+                UserChatRoom.builder().user(opponentUser).chatRoom(expectedChatRoom).chatRoomType(ChatRoomType.GIVEN_OUT).build());
+        when(userChatRoomDao.findByUser(ownUser)).thenReturn(ownUserChatRooms);
+        when(userChatRoomDao.findByUser(opponentUser)).thenReturn(opponentUserChatRooms);
+        // when
+        Optional<ChatRoom> actualChatRoom = chatRoomService.findExistRoom(opponentNickName);
+        // then
+        assertTrue(actualChatRoom.isPresent());
+        assertEquals(expectedChatRoom, actualChatRoom.get());
+    }
+
+    @Test
+    @DisplayName("채팅방 존재 여부 테스트- 채팅방이 존재하지 않는 case")
+    public void findNotExistRoom() {
+        // given
+        String opponentNickName = "opponent";
+        User ownUser = new User();
+        User opponentUser = new User();
+        when(authService.getUserFromSecurity()).thenReturn(ownUser);
+        when(userDao.findById(ownUser.getUserId())).thenReturn(Optional.of(ownUser));
+        when(userDao.findByNickname(opponentNickName)).thenReturn(Optional.of(opponentUser));
+
+        List<UserChatRoom> ownUserChatRooms = Arrays.asList();
+        List<UserChatRoom> opponentUserChatRooms = Arrays.asList();
+        when(userChatRoomDao.findByUser(ownUser)).thenReturn(ownUserChatRooms);
+        when(userChatRoomDao.findByUser(opponentUser)).thenReturn(opponentUserChatRooms);
+        // when
+        Optional<ChatRoom> actualChatRoom = chatRoomService.findExistRoom(opponentNickName);
+        // then
+        assertTrue(actualChatRoom.isEmpty());
     }
 }
