62e4d89fdf2070c9a2c8e95d4bf793889dc998f9
yewonkang00 <yewon00000@naver.com>
2023-01-30 14:35:20 +0900

    [DZ-80] Add : S3 Bucket Setting

---
+++ src/main/java/com/dungzi/backend/global/s3/AmazonS3ResourceStorage.java
@@ -0,0 +1,57 @@
+package com.dungzi.backend.global.s3;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AmazonS3ResourceStorage {
+
+    @Value("${cloud.aws.s3.bucket}")
+
+    //bucketName
+    private final String bucket;
+    private final AmazonS3Client amazonS3Client;
+
+    // 폴더 생성 (폴더는 파일명 뒤에 "/"를 붙여야한다.)
+    public void createFolder(String bucketName, String folderName) {
+        amazonS3Client.putObject(bucketName, folderName + "/", new ByteArrayInputStream(new byte[0]), new ObjectMetadata());
+    }
+    // 파일 업로드
+    public void fileUpload(String fileName, byte[] fileData) throws FileNotFoundException {
+        String filePath = (fileName).replace(File.separatorChar, '/');
+        ObjectMetadata metaData = new ObjectMetadata();
+        metaData.setContentLength(fileData.length);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(fileData);
+
+        try {
+            amazonS3Client.putObject(new PutObjectRequest(bucket, filePath, byteArrayInputStream, metaData));
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
+    }
+
+    // 파일 삭제
+    public void fileDelete(String fileName) {
+
+        System.out.println("fileName : " + fileName);
+        String imgName = (fileName).replace(File.separatorChar, '/');
+
+        try {
+            amazonS3Client.deleteObject(bucket, imgName);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
+        //System.out.println("삭제성공");
+    }
+
+}
