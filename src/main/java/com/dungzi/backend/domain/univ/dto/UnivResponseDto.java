b4b98fb40fac17d9c03675c869fb0d76efd79e72
jeeheaG <jeehea99@naver.com>
2023-02-01 20:21:26 +0900

    [DZ-93] Update : Make toDto() method in dto class instead of constructor

--- src/main/java/com/dungzi/backend/domain/univ/dto/UnivResponseDto.java
+++ src/main/java/com/dungzi/backend/domain/univ/dto/UnivResponseDto.java
@@ -1,6 +1,7 @@
 package com.dungzi.backend.domain.univ.dto;
 
 import com.dungzi.backend.domain.univ.domain.Univ;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 public class UnivResponseDto {
 
+    @Builder
     @Data
     public static class UnivInfo {
         private UUID univId;
@@ -16,17 +18,19 @@ public class UnivResponseDto {
         private String univName;
         private String emailDomain;
 
-        private UnivInfo(Univ univ) {
-            this.univId = univ.getUnivId();
-            this.univName = univ.getUnivName();
-            this.emailDomain = univ.getEmailDomain();
+        private static UnivInfo toDto(Univ univ) {
+            return UnivInfo.builder()
+                    .univId(univ.getUnivId())
+                    .univName(univ.getUnivName())
+                    .emailDomain(univ.getEmailDomain())
+                    .build();
         }
 
         public static List<UnivInfo> toUnivInfoList(List<Univ> univList) {
             List<UnivInfo> univInfoList = new ArrayList<>();
             if(univList.size() != 0) {
                 for (Univ univ : univList) {
-                    univInfoList.add(new UnivInfo(univ));
+                    univInfoList.add(toDto(univ));
                 }
             }
             return univInfoList;
