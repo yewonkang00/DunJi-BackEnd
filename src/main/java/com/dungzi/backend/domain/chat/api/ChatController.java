94de7620cca528a2634f4f0f904822d75ac6c3b2
jeeheaG <jeehea99@naver.com>
2023-02-03 23:18:24 +0900

    !HOTFIX : Fix CORS error 5

--- src/main/java/com/dungzi/backend/domain/chat/api/ChatController.java
+++ src/main/java/com/dungzi/backend/domain/chat/api/ChatController.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("*")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
