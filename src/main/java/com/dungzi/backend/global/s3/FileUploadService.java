75aece1bb9432a1da2f0fc0a1c302bb122d9b2ff
yewonkang00 <yewon00000@naver.com>
2023-02-18 21:47:19 +0900

    [DZ-80] Update : S3버킷 연결 수정

--- src/main/java/com/dungzi/backend/global/s3/FileUploadService.java
+++ src/main/java/com/dungzi/backend/global/s3/FileUploadService.java
@@ -1,58 +1,87 @@
 package com.dungzi.backend.global.s3;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
+@Service
+@RequiredArgsConstructor
 public class FileUploadService {
 
     //bucketName
     @Value("${cloud.aws.s3.bucket}")
-    private static String bucket;
+    private String bucket;
+    private final AmazonS3 amazonS3;
+    private static S3Upload s3Upload;
 
-    private static AmazonS3ResourceStorage amazonS3ResourceStorage;
-    private static String formatName = "jpg"; // 파일 포맷 jpg로 통일
-
-    public static void uploadRoomFile(String RoomID, List<MultipartFile> file) throws Exception {
+    public void uploadRoomFile(String RoomID, List<MultipartFile> file) {
         log.info("[S3] Upload File");
+        String contentType = "image/jpeg";
 
-        String uploadPath = "/room";
+        String uploadPath = bucket + "/room";
+
         for(int i = 0; i < file.size(); i++) {
 
             String fileName;
             if(i == 0) {
-                fileName = '/' + RoomID + '/' + "main" + "." + formatName;
+                fileName = '/' + RoomID + '/' + "main";
             }
             else {
-                fileName = '/' + RoomID + '/' + i + "." + formatName;
+                fileName = '/' + RoomID + '/' + i;
             }
 
-            amazonS3ResourceStorage.fileUpload(bucket+uploadPath+fileName, file.get(i).getBytes());
-            log.info("[S3] Upload File : " + fileName);
+            try {
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentType(contentType);
+
+                amazonS3.putObject(new PutObjectRequest(bucket, uploadPath+fileName, file.get(i).getInputStream(), metadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+
+            } catch (AmazonServiceException | IOException e) {
+                log.error("[S3] Room File Upload Error.");
+                e.printStackTrace();
+            }
         }
     }
 
-    public static String uploadReviewFile(String ReviewID, List<MultipartFile> file) throws Exception {
+    public void uploadReviewFile(String ReviewID, List<MultipartFile> file) {
+        log.info("[S3] Upload File");
+        String contentType = "image/jpeg";
 
-        String uploadPath = "/review";
+        String uploadPath = bucket + "/review";
+
         for(int i = 0; i < file.size(); i++) {
 
             String fileName;
-            fileName = '/' + ReviewID + '/' + i + "." + formatName;
+            if(i == 0) {
+                fileName = '/' + ReviewID + '/' + "main";
+            }
+            else {
+                fileName = '/' + ReviewID + '/' + i;
+            }
 
-            amazonS3ResourceStorage.fileUpload(bucket+uploadPath+fileName, file.get(i).getBytes());
-            log.info("[S3] Upload File : " + fileName);
+            try {
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentType(contentType);
 
+                amazonS3.putObject(new PutObjectRequest(bucket, uploadPath+fileName, file.get(i).getInputStream(), metadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+
+            } catch (AmazonServiceException | IOException e) {
+                log.error("[S3] Review File Upload Error.");
+                e.printStackTrace();
+            }
         }
-
-        String message = "Success";
-
-        return message;
     }
 }
