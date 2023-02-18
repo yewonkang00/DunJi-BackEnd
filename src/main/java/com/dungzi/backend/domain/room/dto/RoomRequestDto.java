076bd56c42c7acec0ec10971cf6194e071c1d2f5
yewonkang00 <yewon00000@naver.com>
2023-02-18 21:54:05 +0900

    [DZ-72] Update : RequestDto 수정

--- src/main/java/com/dungzi/backend/domain/room/dto/RoomRequestDto.java
+++ src/main/java/com/dungzi/backend/domain/room/dto/RoomRequestDto.java
@@ -5,24 +5,20 @@ import com.dungzi.backend.domain.room.domain.RoomAddress;
 import com.dungzi.backend.domain.room.domain.RoomInfo;
 import com.dungzi.backend.domain.room.domain.RoomOption;
 import com.dungzi.backend.domain.user.domain.User;
-import com.dungzi.backend.domain.user.dto.UserDto;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.UUID;
-
+@Slf4j
 public class RoomRequestDto {
 
     @Data
     @Builder
     public static class RegisterDto {
-//        private RoomDto room;
-//        private RoomAddressDto address;
-//        private RoomInfoDto info;
-//        private RoomRequestOptionDto option;
-
-        private UUID roomId;
-        private UUID userId;
+       // private String roomId;
+        private String userId;
+        private Room room;
         private String univId;
         private String title;
         private String content;
@@ -31,69 +27,8 @@ public class RoomRequestDto {
         private double latitude;
         private String address;
         private String addressDetail;
-        private String startedAt;
-        private String finishedAt;
-        private boolean tenancyAgreement;
-        private double roomSize;
-        private int totalFloor;
-        private int floor;
-        private String structure;
-        private String roomType;
-        private String dealType;
-        private int deposit;
-        private int price;
-        private String priceUnit;
-        private int managementCost;
-        private boolean fullOption;
-        private boolean airConditioner;
-        private boolean refrigerator;
-        private boolean washingMachine;
-        private boolean gasStove;
-        private boolean induction;
-        private boolean microwave;
-        private boolean desk;
-        private boolean bookcase;
-        private boolean bed;
-        private boolean closet;
-        private boolean sink;
-        private boolean shoeCabinet;
-        private boolean elevators;
-        private boolean parking;
-        private boolean pets;
-        private boolean womenOnly;
-        private boolean loan;
-        private boolean electricity;
-        private boolean gas;
-        private boolean water;
-        private boolean internet;
-        private boolean tv;
-    }
-
-    @Data
-    @Builder
-    public static class RoomRequestRoomDto {
-        private String roomId;
-        private String userId;
-        private String univId;
-        private String title;
-        private String content;
-        private int image;
-    }
-
-    @Data
-    @Builder
-    public static class RoomRequestAddressDto {
-        private double longtitude;
-        private double latitude;
-        private String address;
-        private String addressDetail;
         private String sigungu;
         private String dong;
-    }
-
-    @Data
-    @Builder
-    public static class RoomRequestInfoDto {
         private String startedAt;
         private String finishedAt;
         private boolean tenancyAgreement;
@@ -108,12 +43,6 @@ public class RoomRequestDto {
         private String priceUnit;
         private int managementCost;
         private boolean fullOption;
-    }
-
-    @Data
-    @Builder
-    public static class RoomRequestOptionDto {
-        private UUID roomId;
         private boolean airConditioner;
         private boolean refrigerator;
         private boolean washingMachine;
@@ -136,7 +65,85 @@ public class RoomRequestDto {
         private boolean water;
         private boolean internet;
         private boolean tv;
+        //private int heartNum;
+        private String status;
+
+        public Room toRoomEntity(User user) {
+//            UUID uuid = null;
+//            if(roomId != null){
+//                uuid = UUID.fromString(roomId);
+//            }
+
+            return Room.builder()
+//                    .roomId(uuid)
+                    .user(user)
+                    .title(title)
+                    .content(content)
+                    .image(image)
+                    .heartNum(0)
+                    .build();
+        }
+
+        public RoomAddress toAddressEntity() {
+         //   log.info("[RoomAddress] roomId : {}", roomId);
+            log.info("[RoomAddress] room : {}", room.getRoomId());
+            return RoomAddress.builder()
+           //         .roomId(roomId)
+                    .room(room)
+                    .longitude(longitude)
+                    .latitude(latitude)
+                    .address(address)
+                    .addressDetail(addressDetail)
+                    .sigungu(sigungu)
+                    .dong(dong)
+                    .status(status)
+                    .build();
+        }
+
+        public RoomInfo toInfoEntity() {
+
+            return RoomInfo.builder()
+             //       .roomId(roomId)
+                    .room(room)
+//                    .startedAt(requestDto.getStartedAt())
+//                    .finishedAt(requestDto.getFinishedAt())
+//                    .tenancyAgreement(requestDto.isTenancyAgreement())
+                    .roomSize(roomSize)
+                    .totalFloor(totalFloor)
+                    .floor(floor)
+                    .structure(structure)
+                    .roomType(roomType)
+                    .dealType(dealType)
+                    .deposit(deposit)
+                    .price(price)
+                    .priceUnit(priceUnit)
+                    .managementCost(managementCost)
+                    .fullOption(fullOption)
+                    .elevators(elevators)
+                    .parking(parking)
+                    .pets(pets)
+                    .womenOnly(womenOnly)
+                    .loan(loan)
+                    .build();
+        }
+
+        public RoomOption toOptionEntity(String option, String utility) {
+            return RoomOption.builder()
+              //      .roomId(roomId)
+                    .room(room)
+                    .options(option)
+                    .utility(utility)
+                    .startedAt(startedAt)
+                    .finishedAt(finishedAt)
+                    .tenancyAgreement(tenancyAgreement)
+                    //.advantage(advantage)
+                    .build();
+        }
+
     }
+
+
+
 
 //    public Room toRoomEntity(RoomDto roomDto) {
 //
@@ -145,7 +152,7 @@ public class RoomRequestDto {
 //        User user = userDto.toEntity();
 //
 //        return Room.builder()
-//                .roomId(UUID.fromString(roomDto.getRoomId()))
+//                .roomId(roomDto.getRoomId())
 //                .user(user)
 ////                .univId(requestDto.getUnivId())
 //                .title(roomDto.getTitle())
@@ -157,46 +164,5 @@ public class RoomRequestDto {
 //                .heartNum(roomDto.getHeartNum())
 //                .build();
 //    }
-//
-//    public RoomAddress toAddressEntity(RegisterDto requestDto) {
-//
-//        return RoomAddress.builder()
-//                .roomId(requestDto.getRoom().getRoomId())
-//                .longtitude(requestDto.getAddress().getLongtitude())
-//                .latitude(requestDto.getAddress().getLatitude())
-//                .address(requestDto.getAddress().getAddress())
-//                .addressDetail(requestDto.getAddress().getAddressDetail())
-//                .build();
-//    }
-//
-//    public RoomInfo toInfoEntity(RoomRequestInfoDto requestDto) {
-//
-//        return RoomInfo.builder()
-//                .roomId(requestDto.getRoomId())
-//                .startedAt(requestDto.getInfo().getStartedAt())
-//                .finishedAt(requestDto.getInfo().getFinishedAt())
-//                .tenancyAgreement(requestDto.getInfo().isTenancyAgreement())
-//                .roomSize(requestDto.getInfo().getRoomSize())
-//                .totalFloor(requestDto.getInfo().getTotalFloor())
-//                .floor(requestDto.getInfo().getFloor())
-//                .structure(requestDto.getInfo().getStructure())
-//                .roomType(requestDto.getInfo().getRoomType())
-//                .dealType(requestDto.getInfo().getDealType())
-//                .deposit(requestDto.getInfo().getDeposit())
-//                .price(requestDto.getInfo().getPrice())
-//                .priceUnit(requestDto.getInfo().getPriceUnit())
-//                .managementCost(requestDto.getInfo().getManagementCost())
-//                .fullOption(requestDto.getInfo().isFullOption())
-//                .build();
-//    }
 
-    public RoomOption toOptionEntity(RoomOptionDto optionDto) {
-
-        return RoomOption.builder()
-                .roomId(optionDto.getRoomId())
-                .options(optionDto.getOptions())
-                .utility(optionDto.getUtility())
-                .advantage(optionDto.getAdvantage())
-                .build();
-    }
 }
