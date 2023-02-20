f09040f6e8e86b259fe20c0134386248b8f840d4
gyuturn <rbtjrrbals@gmail.com>
2023-02-20 21:25:55 +0900

    hotfix: 빌드 에러 수정 및 AWS s3 환경변수 설정

--- src/main/java/com/dungzi/backend/domain/room/application/SetAdvantageConverter.java
+++ src/main/java/com/dungzi/backend/domain/room/application/SetAdvantageConverter.java
@@ -1,16 +1,16 @@
 package com.dungzi.backend.domain.room.application;
 
-import com.dungzi.backend.domain.room.dto.Advantage;
+import com.dungzi.backend.domain.room.dto.enumType.AdvantageEnum;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.AttributeConverter;
 import java.util.Arrays;
 import java.util.EnumSet;
 
-public class SetAdvantageConverter implements AttributeConverter<EnumSet<Advantage>, String>  {
+public class SetAdvantageConverter implements AttributeConverter<EnumSet<AdvantageEnum>, String>  {
 
     @Override
-    public String convertToDatabaseColumn(EnumSet<Advantage> attribute) {
+    public String convertToDatabaseColumn(EnumSet<AdvantageEnum> attribute) {
         // DB에서 사용할 String Type 생성을 위해 StringBuilder 선언
         StringBuilder sb = new StringBuilder();
         // ["KOREA", "ALONE"] 과 같은 EnumSet Collection Data를
@@ -25,16 +25,16 @@ public class SetAdvantageConverter implements AttributeConverter<EnumSet<Advanta
 
     // Entity에서 사용할 Type으로 DB Type을 변환하는 로직 구현
     @Override
-    public EnumSet<Advantage> convertToEntityAttribute(String dbData) {
+    public EnumSet<AdvantageEnum> convertToEntityAttribute(String dbData) {
         // DB에서 읽어온 값이 null이거나 공백이거나 CATEGORY.KOREA(name="한식") 형태로 읽어올 경우 제외
-        if(dbData == null || dbData == "" || dbData.contains(".")) return EnumSet.noneOf(Advantage.class);
+        if(dbData == null || dbData == "" || dbData.contains(".")) return EnumSet.noneOf(AdvantageEnum.class);
         // 최초 빈 Collection 생성
-        EnumSet<Advantage> attribute = EnumSet.noneOf(Advantage.class);
+        EnumSet<AdvantageEnum> attribute = EnumSet.noneOf(AdvantageEnum.class);
         // DB에서 읽어온 "KOREA,ALONE" 형태의 데이터 ','로 split
         String[] dbDataArray = StringUtils.trimAllWhitespace(dbData).toUpperCase().split(",");
         // 빈 Collection으로 생성한 EnumSet에 split한 data를 Category(Enum) .valueOf로 생성
         // 해당 구문에서 Enum에 선언되지 않은 값 존재 시 Exception 발생 가능
-        Arrays.stream(dbDataArray).forEach(e -> attribute.add(Advantage.valueOf(e)));
+        Arrays.stream(dbDataArray).forEach(e -> attribute.add(AdvantageEnum.valueOf(e)));
         return attribute;
     }
 }
--- src/main/java/com/dungzi/backend/domain/room/application/SetOptionConverter.java
+++ src/main/java/com/dungzi/backend/domain/room/application/SetOptionConverter.java
@@ -1,10 +1,10 @@
 package com.dungzi.backend.domain.room.application;
 
-import com.dungzi.backend.domain.room.dto.Options;
+import com.dungzi.backend.domain.room.dto.enumType.OptionsEnum;
+import javax.persistence.AttributeConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
-import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.util.Arrays;
 import java.util.EnumSet;
@@ -13,11 +13,11 @@ import java.util.EnumSet;
 @Converter
 // Entity에서 사용할 X Type으로 EnumSet<Category>
 // DB에서 읽어오거나 저장할 때 사용할 Y Type으로 String 선언
-public class SetOptionConverter implements AttributeConverter<EnumSet<Options>, String> {
+public class SetOptionConverter implements AttributeConverter<EnumSet<OptionsEnum>, String> {
 
     // DB에서 사용할 Type으로 Entity Type을 변환하는 로직 구현
     @Override
-    public String convertToDatabaseColumn(EnumSet<Options> attribute) {
+    public String convertToDatabaseColumn(EnumSet<OptionsEnum> attribute) {
         // DB에서 사용할 String Type 생성을 위해 StringBuilder 선언
         StringBuilder sb = new StringBuilder();
         // ["KOREA", "ALONE"] 과 같은 EnumSet Collection Data를
@@ -32,16 +32,16 @@ public class SetOptionConverter implements AttributeConverter<EnumSet<Options>, 
 
     // Entity에서 사용할 Type으로 DB Type을 변환하는 로직 구현
     @Override
-    public EnumSet<Options> convertToEntityAttribute(String dbData) {
+    public EnumSet<OptionsEnum> convertToEntityAttribute(String dbData) {
         // DB에서 읽어온 값이 null이거나 공백이거나 CATEGORY.KOREA(name="한식") 형태로 읽어올 경우 제외
-        if(dbData == null || dbData == "" || dbData.contains(".")) return EnumSet.noneOf(Options.class);
+        if(dbData == null || dbData == "" || dbData.contains(".")) return EnumSet.noneOf(OptionsEnum.class);
         // 최초 빈 Collection 생성
-        EnumSet<Options> attribute = EnumSet.noneOf(Options.class);
+        EnumSet<OptionsEnum> attribute = EnumSet.noneOf(OptionsEnum.class);
         // DB에서 읽어온 "KOREA,ALONE" 형태의 데이터 ','로 split
         String[] dbDataArray = StringUtils.trimAllWhitespace(dbData).toUpperCase().split(",");
         // 빈 Collection으로 생성한 EnumSet에 split한 data를 Category(Enum) .valueOf로 생성
         // 해당 구문에서 Enum에 선언되지 않은 값 존재 시 Exception 발생 가능
-        Arrays.stream(dbDataArray).forEach(e -> attribute.add(Options.valueOf(e)));
+        Arrays.stream(dbDataArray).forEach(e -> attribute.add(OptionsEnum.valueOf(e)));
         return attribute;
     }
 }
