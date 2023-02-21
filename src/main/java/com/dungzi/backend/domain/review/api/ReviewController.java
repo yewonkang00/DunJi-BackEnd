c521003627c691ef7d521473c6c410101b1096fd
김예린 <yerin1198@naver.com>
2023-02-21 18:00:04 +0900

    [DZ-102] Add: 후기 등록 swagger 구현

--- src/main/java/com/dungzi/backend/domain/review/api/ReviewController.java
+++ src/main/java/com/dungzi/backend/domain/review/api/ReviewController.java
@@ -4,6 +4,9 @@ import com.dungzi.backend.domain.review.application.ReviewService;
 import com.dungzi.backend.domain.review.dto.ReviewRequestDto;
 import com.dungzi.backend.global.common.CommonCode;
 import com.dungzi.backend.global.common.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +19,17 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/review")
+@RequestMapping("/api/v1/review")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @Operation(summary = "후기 생성 api", description = "후기 생성을 위한 api")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "정상적으로 후기 등록 완료")
+            }
+    )
     @PostMapping("/")
     public CommonResponse createReview(@RequestBody ReviewRequestDto.CreateReview requestDto){
         UUID buildingId = reviewService.saveReview(requestDto);
