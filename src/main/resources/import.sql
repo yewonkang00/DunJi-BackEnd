29536eeae62279d368deffa5c4a7de29e7a11c66
jeeheaG <jeehea99@naver.com>
2023-02-27 16:41:50 +0900

    Feat : modify import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- 아래 sql 문이 ddl-auto: create 모드 시 실행됩니다
-insert into univ(univ_Id, univ_name, email_domain) values ('U0001', '한양대학교 에리카', 'hanyang.ac.kr');
+-- 네이버대학교는 개발편의를 위한 임시값입니다.
+insert into univ(univ_Id, univ_name, email_domain) values ('U0001', '한양대학교 에리카', 'hanyang.ac.kr'), ('U9999', '네이버대학교', 'naver.com');
